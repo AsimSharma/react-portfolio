9f8e8e6ebfd2d907658ff530f453e53e89f2e66b
Asim-sharma <80588327+AsimSharma@users.noreply.github.com>
2023-07-24 07:44:21 +0545

    sectionchange

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,79 +3,97 @@ import bgimage from "../components/assets/resize-16896634701287024831HeaderImage
 import bgimage1 from "./assets/download.png"
 import bgimage2 from "./assets/images.png"
 const Portfolio = () => {
-  return (
-    <>
-    <div className='w-full h-screen bg-gradient-to-b from-black  to-gray-800 text-white via-black px-4 py-2 flex flex-col justify-center items-start md:h-screen '>
-    <h1 className='mx-9 font-bold inline border-b-4 border-gray-500 text-4xl'>My Projects</h1>
-    <div className='py-4 m-4 flex justify-between items-center '>
-   
-    <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg bg-white py-6 pb-5 mx-6 cursor-pointer hover:scale-105 duration-150 sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
-    <img className="" src={bgimage} alt="" />
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">title</div>
-      <p className="text-gray-700 text-base">Nepali Market</p>
-    </div>
-    <div className="px-6 py-4">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-       Buy and sell 
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-       visit
-      </span>
-    </div>
-    </div>
- 
-  <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg bg-white py-6 pb-5 cursor-pointer hover:scale-105 duration-150 sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
-  <img className="" src={bgimage1} alt="" />
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">title</div>
-    <p className="text-gray-700 text-base">Food garage</p>
-  </div>
-  <div className="px-6 py-4">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-      Delivered food
-    </span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-     visit
-    </span>
-  </div>
-</div>
+  // const projects=[
+  //   {
+  //     id:1,
+  //     Name:"Nepali Market",
+  //     Description:"Buy and sell product",
+  //     src: bgimage
+  //   },
+  //   {
+  //     id:1,
+  //     Name:"Food Garage",
+  //     Description:"Delivery food online",
+  //     src:bgimage1
+  //   },
+  //   {
+  //     id:1,
+  //     Name:"Attendence System",
+  //     Description:"Attend student in school online",
+  //    src:bgimage1
+  //   },
+  //   {
+  //     id:1,
+  //     Name:"Nepali Market",
+  //     Description:"Buy and sell product",
+  //     src:bgimage
+  //   },
+  // ]
+return(
 
-<div className="max-w-sm rounded-3xl overflow-hidden shadow-lg bg-white py-6  mx-6 cursor-pointer hover:scale-105 duration-150 sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0  ">
-<img className="" src={bgimage2} alt="" />
-<div className="px-6 py-4">
-  <div className="font-bold text-xl mb-2">title</div>
-  <p className="text-gray-700 text-base">Attendance System</p>
-</div>
-<div className="px-6 py-4">
-  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-    for School attendance
-  </span>
-  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-   visit
-  </span>
-</div>
+  <section className="bg-gradient-to-b from-black to-gray-800 w-full h-auto py-8 lg:h-screen">
+      <div className="container mx-auto px-4 ">
+        <h1 className="text-2xl font-bold mb-4 text-white">my projects</h1>
+       
+        <div className="grid grid-cols-2 gap-8 lg:grid-cols-4 py-2 ">
+          {/* Add your product cards here */}
+          {/* Example card */}
+          <div className="bg-gradient-to-b from-slate-600 to-white shadow-md rounded-lg p-4 hover:scale-105 duration-150 cursor-pointer">
+           <img
+              src={bgimage}
+              alt="project"
+              className="w-full h-32 object-cover mb-4 rounded"
+            />
+            <h3 className="text-xl font-bold">Nepali Market</h3>
+            <p className="text-gray-600">Product description goes here.</p>
+            <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-slate-600 scale-90 duration-150">
+              Visit
+            </button>
+          </div>
+          <div className="bg-gradient-to-b from-slate-600 to-white shadow-md rounded-lg p-4 hover:scale-105 duration-150 cursor-pointer">
+          <img
+             src={bgimage2}
+             alt="project"
+             className="w-full h-32 object-cover mb-4 rounded"
+           />
+           <h3 className="text-xl font-bold">Attendance sytem</h3>
+           <p className="text-gray-600">School student attendence system</p>
+           <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md  hover:bg-slate-600 scale-90 duration-150">
+             Visit
+           </button>
+         </div>
+         <div className="bg-gradient-to-b from-slate-600 to-white shadow-md rounded-lg p-4 hover:scale-105 duration-150 cursor-pointer">
+         <img
+            src={bgimage1}
+            alt="project"
+            className="w-full h-32 object-cover mb-4 rounded"
+          />
+          <h3 className="text-xl font-bold">Food garage</h3>
+          <p className="text-gray-600">Delivered food online </p>
+          <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md  hover:bg-slate-600 scale-90 duration-150">
+            Visit
+          </button>
+        </div>
+        <div className="bg-gradient-to-b from-slate-600 to-white shadow-md rounded-lg p-4 hover:scale-105 duration-150 cursor-pointer">
+        <img
+           src={bgimage2}
+           alt="project"
+           className="w-full h-32 object-cover mb-4 rounded"
+         />
+         <h3 className="text-xl font-bold">Attendance sytem</h3>
+         <p className="text-gray-600">School student attendence system</p>
+         <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md  hover:bg-slate-600 scale-90 duration-150">
+           Visit
+         </button>
+       </div>
+          
+          
+        </div>
+        
+      </div>
+    </section>
 
-</div>
-<div className="max-w-sm rounded-3xl overflow-hidden shadow-lg bg-white py-6 pb-5 cursor-pointer hover:scale-105 duration-150 sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
-  <img className="" src={bgimage1} alt="" />
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">title</div>
-    <p className="text-gray-700 text-base">Food garage</p>
-  </div>
-  <div className="px-6 py-4">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-      Delivered food
-    </span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-     visit
-    </span>
-  </div>
-</div>
-</div>
-  </div>
-  </>
-  )
+)
 }
 
 export default Portfolio
